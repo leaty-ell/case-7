4c4157c2965507baf3f7bff93e763ce5e534f4e7
leaty-ell <lisalimanova14@gmail.com>
2025-09-30 23:06:58 +0700

    Create ru_local.py

---
+++ ru_local.py
@@ -0,0 +1,22 @@
+NOT_SPECIFIED = "Не указано"
+PROCESSED_PRODUCT = "Обработан товар"
+FILE_SAVED = "Файл успешно сохранен"
+TOTAL_PROCESSED = "Всего обработано товаров"
+NO_PRODUCTS_FOUND = "Товары не найдены"
+
+
+NAME = "Наименование"
+PRICE = "Цена"
+SIZES = "Размеры"
+MATERIAL = "Материал верха"
+ARTICLE = "Артикул"
+TYPE = "Вид обуви"
+SEASON = "Сезон"
+COLOR = "Цвет"
+COUNTRY = "Страна"
+
+
+PRICE_NUM = "Цена_число"
+
+
+FILE_NAME = "обувь_товары.xlsx"
